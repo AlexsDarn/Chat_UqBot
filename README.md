3199bad64ac519f7fc26a362230020c3bd02338a
AlexsDarn <alexisd4rk@gmail.com>
2023-04-19 10:54:37 -0500

    uqbot 0.6

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
-# UQBot
+Librerías:
 
-Instalación de librerías
+Random, Json, Torch, Flask, NeuralNet, Numpy, Nltk
 
-pip install Flask torch torchvision nltk
+Para ejecutar la aplicación: En consola
 
-Para ejecutar la aplicación: 
-python3 train.py
-python3 app.py
+python3 train.py python3 chat.py
 
-Luego se dirige a front/base.html, click derechi y live server 
-(Si no encuentra la opción, debe entrar descarga la extensión en visual studio code)
+luego abrir: http://127.0.0.1:5000/
 
-Para agregar un enlace en una respuesta de intents.json,
-utilice este ejemplo:
-<a target='_blank' href='#'>#</a>
+Para agregar un enlace en una respuesta de intents.json, utilice este ejemplo: #
 
-references
-https://www.youtube.com/watch?v=a37BL0stIuM
-https://github.com/patrickloeber/chatbot-deployment
+references https://www.youtube.com/watch?v=a37BL0stIuM https://github.com/patrickloeber/chatbot-deployment
--- chat.py
+++ chat.py
@@ -31,9 +31,11 @@ questions = ["Hola", "Que Tal?"]
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/get')
 def get_bot_response():
@@ -41,13 +43,33 @@ def get_bot_response():
     userText = request.args.get('msg')
     questions.append(userText)
     response = get_response(userText)
+    print(questions)
+
     return str(response)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     userText = request.json['message']
     response = get_response(userText)
     return {'answer': response}
+
+
+def add_intent(question, response):
+    with open('intents.json', 'r') as file:
+        intents = json.load(file)
+
+    # Agregar una nueva entrada
+    intents['intents'].append({
+        'tag': 'custom',
+        'patterns': [question],
+        'responses': [response]
+    })
+
+    # Escribir el archivo de vuelta
+    with open('intents.json', 'w') as file:
+        json.dump(intents, file, indent=4)
+
 
 @app.route('/stats')
 def get_stats():
@@ -59,15 +81,16 @@ def get_stats():
             for question in questions:
                 if question == pattern:
                     count += 1
-            if count > 0:
-                question_counts[pattern] = count
+                if count > 0:
+                    question_counts[pattern] = count
+
     sorted_questions = sorted(question_counts.items(), key=lambda x: x[1], reverse=True)
 
     return render_template('stats.html', stats=sorted_questions)
 
 
-
 def get_response(msg):
+    global intents  # Agregar esta línea para importar la variable global
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -80,11 +103,17 @@ def get_response(msg):
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
+
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
-    
+    else:
+        # Si la confianza del modelo es baja, asumimos que es una pregunta nueva
+        add_intent(msg, "Lo siento, no sé la respuesta a esa pregunta")
+        questions.append(msg)
+        intents = json.load(open('intents.json'))
+
     return "No entiendo :("
 
 
